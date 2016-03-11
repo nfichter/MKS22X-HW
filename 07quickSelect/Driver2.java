77b16499af99af2700f1f11c642832163f8fa043
Noah Fichter <nfichter@stuy.edu>
2016-03-11 12:22:51 -0500

    Began updated quickSort

---
+++ 07quickSelect/Driver2.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+public class Driver2{
+    public static int[] fillRandom(int[] ary){
+	for (int i = 0; i < ary.length; i++){
+	    ary[i] = (int)(Math.random() * 10000 / (Math.random() * 100));
+	}
+        return ary;
+    }
+    public static int[] fillRandom3(int[] ary) {
+	for (int i = 0; i < ary.length; i++) {
+	    ary[i] = (int)(Math.random() * 3);
+	}
+	return ary;
+    }
+
+    public static void main(String[]args){
+        int size = 10000;
+        String choice = "quick";
+
+        if(args.length >= 1){
+            choice = args[0];
+        }
+
+        if(args.length >= 2){ //pick number of elements
+            size = Integer.parseInt(args[1]);
+        }
+
+	int[] ary = new int[10000];
+	int[] ary1 = new int[10000];
+	int[] arraysary = new int[10000];
+	fillRandom3(ary);
+	fillRandom3(ary1);
+	fillRandom3(arraysary);
+
+	long startarrays = System.currentTimeMillis();
+	Arrays.sort(arraysary);
+	long endarrays = System.currentTimeMillis();
+
+	System.out.println("Arrays.sort - Time:" + (endarrays-startarrays)/1000.0 + " seconds. Size = "+arraysary.length);
+
+
+        long start = System.currentTimeMillis();
+        if(choice.equals("quick")){
+            Quick.quickSortUno(ary);
+        }
+        long end = System.currentTimeMillis();
+
+        System.out.println("Sorts.quickSortUno - Time:"+ (end-start)/1000.0 + " seconds. Size = "+ary.length + " Sorted? " + Arrays.equals(arraysary,ary));
+
+
+	long start1 = System.currentTimeMillis();
+	if(choice.equals("quick")){
+            Quick.quickSort(ary1);
+        }
+        long end1 = System.currentTimeMillis();
+
+        System.out.println("Sorts.quickSort - Time:"+ (end1-start1)/1000.0 + " seconds. Size = "+ary1.length + " Sorted? " + Arrays.equals(arraysary,ary1));
+    }
+}
--- 07quickSelect/Quick.java
+++ 07quickSelect/Quick.java
@@ -1,90 +1,172 @@
 import java.util.*;
 
 public class Quick {
-	private static void swap(int[] data, int firstIndex, int secondIndex) {
-		int hold = data[firstIndex];
-		data[firstIndex] = data[secondIndex];
-		data[secondIndex] = hold;
-	}
+    private static void swap(int[] data, int firstIndex, int secondIndex) {
+	int hold = data[firstIndex];
+	data[firstIndex] = data[secondIndex];
+	data[secondIndex] = hold;
+    }
 	
-	private static int partition(int[] data, int left, int right) {
-	    int index = (int)(Math.random()*(right-left)) + left;
-		int num = data[index];
+    private static int partitionUno(int[] data, int left, int right) {
+	int index = (int)(Math.random()*(right-left)) + left;
+	int num = data[index];
 		
-		swap(data,index,left);
+	swap(data,index,left);
 		
-		int cLeft = left+1;
-		int cRight = right;
+	int cLeft = left+1;
+	int cRight = right;
 		
-		while(cRight > cLeft) {
-			if (data[cLeft] > num) {
-			    swap(data,cLeft,cRight);
-			    cRight--;
-			}
-			else {
-			    cLeft++;
-			}
-		}
-		
-	        if (data[cRight] > data[left]) {
-		    swap(data,left,cRight-1);
-		    index = cRight-1;
-		}
-		else {
-		    swap(data,left,cRight);
-		    index = cRight;
-		}
-		
-		return index;
+	while(cRight > cLeft) {
+	    if (data[cLeft] > num) {
+		swap(data,cLeft,cRight);
+		cRight--;
+	    }
+	    else {
+		cLeft++;
+	    }
 	}
+		
+	if (data[cRight] > data[left]) {
+	    swap(data,left,cRight-1);
+	    index = cRight-1;
+	}
+	else {
+	    swap(data,left,cRight);
+	    index = cRight;
+	}
+		
+	return index;
+    }
 	
-	private static int quickselect(int[] data, int k, int left, int right) {
-		int index = partition(data,left,right);
+    private static int quickselectUno(int[] data, int k, int left, int right) {
+	int index = partitionUno(data,left,right);
 		
-		if (index == k) {
-			return data[k];
-		}
+	if (index == k) {
+	    return data[k];
+	}
 		
-		if (index >= k) {
-			return quickselect(data,k,left,index);
-		}
+	if (index >= k) {
+	    return quickselectUno(data,k,left,index);
+	}
 		
-		else {
-			return quickselect(data,k,index,right);
+	else {
+	    return quickselectUno(data,k,index,right);
+	}
+    }
+	
+    public static int quickselectUno(int[] data, int k) {
+	return quickselectUno(data,k,0,data.length-1);
+    }
+	
+    public static void quickSortUno(int[] data, int left, int right) {
+	if (right-left >= 1) {
+	    int index = partitionUno(data,left,right);
+	    quickSortUno(data,left,index-1);
+	    quickSortUno(data,index+1,right);
+	}
+    }
+	
+    public static void quickSortUno(int[] data) {
+	quickSortUno(data,0,data.length-1);
+    }
+
+    private static int partition(int[] data, int left, int right) {
+	int index = (int)(Math.random()*(right-left)) + left;
+	int num = data[index];
+		
+	swap(data,index,left);
+		
+	int cLeft = left+1;
+	int cRight = right;
+
+	while(cRight > cLeft) {
+	    if (data[cLeft] > num) {
+		swap(data,cLeft,cRight);
+		cRight--;
+	    }
+	    else {
+		cLeft++;
+	    }
+	}
+
+	int swapperR = index+1;
+	int swapperL = index-1;
+
+	for (int i = 0; i < data.length; i++) {
+	    if (data[i] == num) {
+		if (swapperR < data.length) {
+		    swap(data,i,swapperR);
+		    if (data[i] == num) {
+			i--;
+		    }
+		    swapperR++;
 		}
-	}
-	
-	public static int quickselect(int[] data, int k) {
-		return quickselect(data,k,0,data.length-1);
-	}
-	
-	public static void quickSort(int[] data, int left, int right) {
-		if (right-left >= 1) {
-			int index = partition(data,left,right);
-			quickSort(data,left,index-1);
-			quickSort(data,index+1,right);
+		else if (swapperL > 0) {
+		    swap(data,i,swapperL);
+		    if (data[i] == num) {
+			i--;
+		    }
+		    swapperL--;
 		}
+	    }
 	}
+		
+	if (data[cRight] > data[left]) {
+	    swap(data,left,cRight-1);
+	    index = cRight-1;
+	}
+	else {
+	    swap(data,left,cRight);
+	    index = cRight;
+	}
+		
+	return index;
+    }
 	
-	public static void quickSort(int[] data) {
-		quickSort(data,0,data.length-1);
+    private static int quickselect(int[] data, int k, int left, int right) {
+	int index = partition(data,left,right);
+		
+	if (index == k) {
+	    return data[k];
 	}
+		
+	if (index >= k) {
+	    return quickselect(data,k,left,index);
+	}
+		
+	else {
+	    return quickselect(data,k,index,right);
+	}
+    }
 	
-	public static void printArray(int[] data) {
-		for (int i = 0; i < data.length; i++) {
-			System.out.print(data[i] + ", ");
-		}
-		System.out.println();
+    public static int quickselect(int[] data, int k) {
+	return quickselect(data,k,0,data.length-1);
+    }
+	
+    public static void quickSort(int[] data, int left, int right) {
+	if (right-left >= 1) {
+	    int index = partition(data,left,right);
+	    quickSort(data,left,index-1);
+	    quickSort(data,index+1,right);
 	}
+    }
+	
+    public static void quickSort(int[] data) {
+	quickSort(data,0,data.length-1);
+    }
+	
+    public static void printArray(int[] data) {
+	for (int i = 0; i < data.length; i++) {
+	    System.out.print(data[i] + ", ");
+	}
+	System.out.println();
+    }
 
     public static String name() {
 	return "6,Fichter,Noah";
     }
 	
-	public static void main(String[] args) {
-		int[] data = {1, 4, 5, 99, 2, 40, 420, 60, 32, -2};
-		printArray(data);
-	        quickSort(data);
-		printArray(data);
-	}
+    public static void main(String[] args) {
+	
+    }
 }
