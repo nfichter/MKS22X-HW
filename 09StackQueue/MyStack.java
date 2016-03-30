6ad3d287960811bd0fea2ae5cdcf01326f44456b
Noah Fichter <nfichter@stuy.edu>
2016-03-30 12:22:05 -0400

    began testing

--- 09StackQueue/MyStack.java
+++ 09StackQueue/MyStack.java
@@ -36,4 +36,13 @@ public class MyStack<T> {
 	public String toString() {
 	    return MLL.toString();
 	}
+
+	public static void main(String[] args) {
+	    Stack<Integer> s = new Stack<Integer>();
+	    s.push(1);
+	    s.push(2);
+	    s.push(3);
+	    System.out.println(s);
+	    MyStack<Integer> ms = new Stack<Integer>();
+	}
 }
