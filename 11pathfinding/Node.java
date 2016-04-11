ff41db13f190ea6088928ba2fddff2243cea7d52
Noah Fichter <nfichter@stuy.edu>
2016-04-11 11:58:12 -0400

    Added Node class

---
+++ 11pathfinding/Node.java
@@ -0,0 +1,28 @@
+public class Node {
+    Coordinate loc;
+    Node last;
+
+    public Node() {
+	loc = new Coordinate();
+	last = null;
+    }
+    public Node(Coordinate loc) {
+	this.loc = loc;
+	last = null;
+    }
+    public Node(Node last) {
+	loc = new Coordinate();
+	this.last = last;
+    }
+    public Node(Coordinate loc, Node last) {
+	this.loc = loc;
+	this.last = last;
+    }
+
+    public Node getLast() {
+	return last;
+    }
+    public Coordinate getCoord() {
+	return loc;
+    }
+}
